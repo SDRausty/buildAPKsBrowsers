75205ef6ee6b90d0eb4d3ead3c3ab83067044638
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-18 21:19:55 -0400

    commit 131246853 on 20210318

--- ma.bash
+++ ma.bash
@@ -7,5 +7,6 @@ shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
+_AT_ eonj/Android-Resource-Browser 6b56a5090fb67c65a945d29a4bcdbab3cb4d8e69
 _AT_ xdtianyu/MyBrowser 7a6a0188e958c0eb9d01d67710ae1f9cc918aa5a
 # ma.bash OEF
