b91d1368254d066ee63106674ebed0e86b4b3fd9
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-02 12:26:20 -0400

    commit 802523328 on 20210402

--- ma.bash
+++ ma.bash
@@ -10,7 +10,7 @@ cd "$JDR"
 _AT_ Sjith/Magellan a3cfe75ec31c9cff917bd68d9a476a8416f30d89
 _AT_ ch-ms/See 45ee117937edd6b7271e440fc91e97c16c1c1340
 sed -i 's/my awesome blog at //g' ch-ms/See/ch-ms-See-45ee117/assets/about.html
-sed -i 's/forallx.ru/github.com\/buildAPKs/g' ch-ms/See/ch-ms-See-45ee117/assets/about.html
+sed -i 's/forallx.ru/www.github.com\/buildAPKs/g' ch-ms/See/ch-ms-See-45ee117/assets/about.html
 _AT_ eonj/Android-Resource-Browser 6b56a5090fb67c65a945d29a4bcdbab3cb4d8e69
 _AT_ jrudolph/object-browser 9c3adfa87fb3f295f5c50972cea14211d7706b42
 _AT_ kalkin/FileBrowser 714d1e037b88781c40e1d9481c668f69d5096eb2
